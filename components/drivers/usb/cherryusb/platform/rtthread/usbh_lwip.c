22894c12ff4abed4d3c31825eb57ff093a47d8eb
yixinghua <yixinghua@canaan-creative.com>
2024-12-06 17:23:17 +0800

    add usb driver

--- components/drivers/usb/cherryusb/platform/rtthread/usbh_lwip.c
+++ components/drivers/usb/cherryusb/platform/rtthread/usbh_lwip.c
@@ -437,11 +437,28 @@ void usbh_rtl8152_eth_input(uint8_t *buf, uint32_t buflen)
     usbh_lwip_eth_input_common(g_rtl8152_dev.netif, buf, buflen);
 }
 
+#ifdef RT_USING_DEVICE_OPS
+const static struct rt_device_ops rtl8152_device_ops =
+{
+    RT_NULL,
+    RT_NULL,
+    RT_NULL,
+    RT_NULL,
+    RT_NULL,
+    rt_usbh_rtl8152_control
+};
+#endif
+
 void usbh_rtl8152_run(struct usbh_rtl8152 *rtl8152_class)
 {
     memset(&g_rtl8152_dev, 0, sizeof(struct eth_device));
 
+#ifdef RT_USING_DEVICE_OPS
+    g_rtl8152_dev.parent.ops = &rtl8152_device_ops;
+#else
     g_rtl8152_dev.parent.control = rt_usbh_rtl8152_control;
+#endif
+
     g_rtl8152_dev.eth_rx = NULL;
     g_rtl8152_dev.eth_tx = rt_usbh_rtl8152_eth_tx;
     g_rtl8152_dev.parent.user_data = rtl8152_class;
