9aa5fa766f14af845678da15bdb3afcdf55bd0e7
yixinghua <yixinghua@canaan-creative.com>
2025-01-07 14:56:18 +0800

    add timer interrupt send signal to userspace

--- bsp/k230/drivers/interdrv/hwtimer/drv_timer.c
+++ bsp/k230/drivers/interdrv/hwtimer/drv_timer.c
@@ -35,24 +35,17 @@
 #include "sysctl_clk.h"
 #include "sysctl_rst.h"
 #include "drv_timer.h"
+#include <dfs_posix.h>
+#include <lwp_user_mm.h>
 
-/**
- * @brief timer
- * channel-0
- * channel-1
- * channel-2
- * channel-3
- * channel-4
- * channel-5
- */
-
-
-struct kd_timer_Type
-{
+struct kd_timer_Type {
     kendryte_timer_t *base_addr;
     uint32_t id;
+    struct rt_work send_sig_work;
+    struct rt_lwp* lwp;
+    int signo;
+    void* sigval;
 };
-
 
 #ifdef RT_USING_HW_TIMER0
 static rt_hwtimer_t _timer0;
@@ -78,7 +71,6 @@ static struct kd_timer_Type kd_timer4;
 static rt_hwtimer_t _timer5;
 static struct kd_timer_Type kd_timer5;
 #endif
-
 
 static void kd_timer_init(rt_hwtimer_t *timer, rt_uint32_t state)
 {
@@ -192,13 +184,33 @@ static rt_uint32_t kd_timer_get(rt_hwtimer_t *timer)
     return reg->channel[id].current_value;
 }
 
+static void send_sig_work(struct rt_work* work, void* param)
+{
+    lwp_siginfo_ext_t ext;
+    struct kd_timer_Type *kd_timer = param;
+
+    ext = rt_malloc(sizeof(struct lwp_siginfo_ext));
+    if (ext)
+        ext->sigval.sival_ptr = kd_timer->sigval;
+    lwp_signal_kill(kd_timer->lwp, kd_timer->signo, SI_SIGIO, ext);
+}
+
+static rt_err_t timer_timeout_ind(rt_device_t dev, rt_size_t size)
+{
+    rt_hwtimer_t *timer = (rt_hwtimer_t *)dev;
+    struct kd_timer_Type *kd_timer = (struct kd_timer_Type *)timer->parent.user_data;
+
+    rt_work_init(&kd_timer->send_sig_work, send_sig_work, kd_timer);
+    rt_work_submit(&kd_timer->send_sig_work, 0);
+}
+
 static rt_err_t kd_timer_ctrl(rt_hwtimer_t *timer, rt_uint32_t cmd, void *arg)
 {
     struct kd_timer_Type *kd_timer = (struct kd_timer_Type *)timer->parent.user_data;
-    uint8_t id = kd_timer->id;
-    timer->freq = *((rt_uint32_t*)arg);
-    if (cmd == HWTIMER_CTRL_FREQ_SET)
-    {
+
+    if (cmd == HWTIMER_CTRL_FREQ_SET) {
+        uint8_t id = kd_timer->id;
+        timer->freq = *((rt_uint32_t*)arg);
         switch(id)
         {
         case 0:
@@ -271,11 +283,40 @@ static rt_err_t kd_timer_ctrl(rt_hwtimer_t *timer, rt_uint32_t cmd, void *arg)
             break;
         }
         return RT_EOK;
+    } else if (cmd == HWTIMER_CTRL_IRQ_SET) {
+        hwtimer_irqcfg_t cfg;
+        lwp_get_from_user(&cfg, arg, sizeof(cfg));
+        rt_base_t level = rt_hw_interrupt_disable();
+        if (cfg.enable) {
+            struct rt_lwp* lwp = lwp_self();
+            kd_timer->lwp = lwp;
+            kd_timer->signo = cfg.signo;
+            kd_timer->sigval = cfg.sigval;
+            rt_device_set_rx_indicate(&timer->parent, timer_timeout_ind);
+        } else {
+            rt_device_set_rx_indicate(&timer->parent, NULL);
+        }
+        rt_hw_interrupt_enable(level);
     }
 
     return RT_EOK;
 }
 
+static int timer_fops_open(struct dfs_file* fd)
+{
+    rt_device_t device = (rt_device_t)fd->vnode->data;
+    int ret = rt_device_open(device, RT_DEVICE_OFLAG_RDWR);
+
+    return ret;
+}
+
+static int timer_fops_close(struct dfs_file* fd)
+{
+    rt_device_t device = (rt_device_t)fd->vnode->data;
+    int ret = rt_device_close(device);
+
+    return ret;
+}
 
 static const struct rt_hwtimer_info _info =
 {
@@ -294,6 +335,10 @@ static const struct rt_hwtimer_ops _ops =
     .control = kd_timer_ctrl,
 };
 
+static const struct dfs_file_ops timer_fops = {
+    timer_fops_open,
+    timer_fops_close,
+};
 
 void kd_hwtimer_isr(int vector, void *param)
 {
@@ -311,7 +356,6 @@ void kd_hwtimer_isr(int vector, void *param)
     rt_device_hwtimer_isr(hwtimer);
 }
 
-
 int rt_hw_timer_init(void)
 {
     volatile void* addr =  (void *)rt_ioremap((void *)HW_TIMER_BASE_ADDR, HW_TIMER_IO_SIZE);
@@ -323,11 +367,9 @@ int rt_hw_timer_init(void)
     _timer0.info = &_info;
     _timer0.ops = &_ops;
     rt_device_hwtimer_register(&_timer0, "hwtimer0", &kd_timer0);
+    _timer0.parent.fops = &timer_fops;
     rt_hw_interrupt_install(IRQN_TIMER_0_INTERRUPT, kd_hwtimer_isr, &_timer0, "hwtimer0");
     rt_hw_interrupt_umask(IRQN_TIMER_0_INTERRUPT);
-#ifndef RT_FASTBOOT
-    rt_kprintf("timer0 register OK\n");
-#endif
 #endif
 
 #ifdef RT_USING_HW_TIMER1
@@ -337,11 +379,9 @@ int rt_hw_timer_init(void)
     _timer1.info = &_info;
     _timer1.ops = &_ops;
     rt_device_hwtimer_register(&_timer1, "hwtimer1", &kd_timer1);
+    _timer1.parent.fops = &timer_fops;
     rt_hw_interrupt_install(IRQN_TIMER_1_INTERRUPT, kd_hwtimer_isr, &_timer1, "hwtimer1");
     rt_hw_interrupt_umask(IRQN_TIMER_1_INTERRUPT);
-#ifndef RT_FASTBOOT
-    rt_kprintf("timer1 register OK\n");
-#endif
 #endif
 
 #ifdef RT_USING_HW_TIMER2
@@ -351,11 +391,9 @@ int rt_hw_timer_init(void)
     _timer2.info = &_info;
     _timer2.ops = &_ops;
     rt_device_hwtimer_register(&_timer2, "hwtimer2", &kd_timer2);
+    _timer2.parent.fops = &timer_fops;
     rt_hw_interrupt_install(IRQN_TIMER_2_INTERRUPT, kd_hwtimer_isr, &_timer2, "hwtimer2");
     rt_hw_interrupt_umask(IRQN_TIMER_2_INTERRUPT);
-#ifndef RT_FASTBOOT
-    rt_kprintf("timer2 register OK\n");
-#endif
 #endif
 
 #ifdef RT_USING_HW_TIMER3
@@ -365,11 +403,9 @@ int rt_hw_timer_init(void)
     _timer3.info = &_info;
     _timer3.ops = &_ops;
     rt_device_hwtimer_register(&_timer3, "hwtimer3", &kd_timer3);
+    _timer3.parent.fops = &timer_fops;
     rt_hw_interrupt_install(IRQN_TIMER_3_INTERRUPT, kd_hwtimer_isr, &_timer3, "hwtimer3");
     rt_hw_interrupt_umask(IRQN_TIMER_3_INTERRUPT);
-#ifndef RT_FASTBOOT
-    rt_kprintf("timer3 register OK\n");
-#endif
 #endif
 
 #ifdef RT_USING_HW_TIMER4
@@ -379,11 +415,9 @@ int rt_hw_timer_init(void)
     _timer4.info = &_info;
     _timer4.ops = &_ops;
     rt_device_hwtimer_register(&_timer4, "hwtimer4", &kd_timer4);
+    _timer4.parent.fops = &timer_fops;
     rt_hw_interrupt_install(IRQN_TIMER_4_INTERRUPT, kd_hwtimer_isr, &_timer4, "hwtimer4");
     rt_hw_interrupt_umask(IRQN_TIMER_4_INTERRUPT);
-#ifndef RT_FASTBOOT
-    rt_kprintf("timer4 register OK\n");
-#endif
 #endif
 
 #ifdef RT_USING_HW_TIMER5
@@ -393,11 +427,9 @@ int rt_hw_timer_init(void)
     _timer5.info = &_info;
     _timer5.ops = &_ops;
     rt_device_hwtimer_register(&_timer5, "hwtimer5", &kd_timer5);
+    _timer5.parent.fops = &timer_fops;
     rt_hw_interrupt_install(IRQN_TIMER_5_INTERRUPT, kd_hwtimer_isr, &_timer5, "hwtimer5");
     rt_hw_interrupt_umask(IRQN_TIMER_5_INTERRUPT);
-#ifndef RT_FASTBOOT
-    rt_kprintf("timer5 register OK\n");
-#endif
 #endif
 }
 INIT_BOARD_EXPORT(rt_hw_timer_init);
