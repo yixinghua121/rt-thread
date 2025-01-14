6e672c451ad181189ee7e5a1e22e064281da3e55
yixinghua <yixinghua@canaan-creative.com>
2025-01-14 17:01:21 +0800

    add usage driver

--- bsp/k230/rtconfig.py
+++ bsp/k230/rtconfig.py
@@ -14,7 +14,7 @@ if os.getenv('RTT_CC'):
 
 if  CROSS_TOOL == 'gcc':
     PLATFORM    = 'gcc'
-    EXEC_PATH   = r'/opt/toolchain/rt-smart_v13.2.0-release_1109/bin'
+    EXEC_PATH   = r'/opt/toolchain/riscv64-linux-musleabi_for_x86_64-pc-linux-gnu_rtt/bin'
 else:
     print('Please make sure your toolchains is GNU GCC!')
     exit(0)
